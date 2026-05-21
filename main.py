d3c51caf5e04307db262cd63972a5a69ef38cfe8
My-Arsenal <39475759+Zombie1331240@users.noreply.github.com>
2026-05-22 00:58:42 +0700

    Fix capitalization of 'Port' in app.run

--- main.py
+++ main.py
@@ -9,4 +9,4 @@ def home():
 
 if __name__ == '__main__':
     # บรรทัดนี้คือหัวใจสำคัญที่ทำให้ Cloud รู้จัก Port เจ้าค่ะ
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
+    app.run(host='0.0.0.0', Port=int(os.environ.get('PORT', 8080)))
