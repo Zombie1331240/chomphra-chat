7f6916b0295afd46ae970db940497b305ceb6c14
My-Arsenal <39475759+Zombie1331240@users.noreply.github.com>
2026-05-22 01:45:57 +0700

    Fix typo in comment and parameter name for app.run

--- main.py
+++ main.py
@@ -8,5 +8,5 @@ def home():
     return "Chomphra-chat กำลังทำงานบนคลาวด์!"
 
 if __name__ == '__main__':
-    # บรรทัดนี้คือหัวใจสำคัญที่ทำให้ Cloud รู้จัก Port เจ้าค่ะ
-    app.run(host='0.0.0.0', Port=int(os.environ.get('PORT', 8080)))
+    # บรรทัดนี้คือหัวใจสำคัญที่ทำให้ Cloud รู้จัก port เจ้าค่ะ
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
