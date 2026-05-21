963cc80c1ec8517bbd825ea08df266dfd5433001
My-Arsenal <39475759+Zombie1331240@users.noreply.github.com>
2026-05-22 00:40:25 +0700

    Update home route message in main.py

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "อาณาจักร Chomphra-chat กำลังปฏิบัติการ!"
+    return "Chomphra-chat กำลังทำงานบนคลาวด์!"
 
 if __name__ == '__main__':
+    # บรรทัดนี้คือหัวใจสำคัญที่ทำให้ Cloud รู้จัก Port เจ้าค่ะ
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
